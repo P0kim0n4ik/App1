825e856f62fbb4dfa60de469d9616b79072b22b9
Алексей Стрелов <StrelovAD@chgtk.ru>
2021-06-18 11:52:11 +0800

    Добавьте файлы проекта.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MySqlConnector;
+using System;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class App1 : Application
+    {
+        public App1()
+        {
+            InitializeComponent();
+
+            string name = "";
+            string fffge = "server=10.0.0.34;user=strelovad;password=eXmOx2Z)Sg886ttG;database=strelovad";
+            string nameSQL = $"SELECT * FROM `films` WHERE id = `1`";
+            var connect = new MySqlConnection(fffge);
+            connect.Open();
+            var command = new MySqlCommand(nameSQL, connect);
+            var read = command.ExecuteReader();
+            if (read.Read())
+            {
+                name = read.GetString(1);
+                byte[] pic = read.GetValue(2) as byte[];
+            }
+
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ App1/App1/Page1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+          
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+        }
+
+
+        private async void next(object btn, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
